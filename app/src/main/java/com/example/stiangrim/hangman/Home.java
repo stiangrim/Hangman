6198a02b3ccf687ff96f448d4186068310540ac4
stgr99 <stian.tornholm.grimsgaard@protectorforsikring.no>
2017-09-25 12:21:53 +0200

    Final Changes

--- app/src/main/java/com/example/stiangrim/hangman/Home.java
+++ app/src/main/java/com/example/stiangrim/hangman/Home.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.View;
@@ -84,8 +85,7 @@ public class Home extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        finish();
-        System.exit(0);
+        ActivityCompat.finishAffinity(this);
     }
 
 }
